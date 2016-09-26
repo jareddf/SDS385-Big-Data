4ab3fa2fe4c15002023dc60947610a6bb8433f94
jareddf <jared.fisher@phd.mccombs.utexas.edu>
2016-09-26 09:45:58 -0500

    Update

--- ex02-functions.R
+++ ex02-functions.R
@@ -1,25 +1,29 @@
+# Functions needed for Exercises 02, SDS 385 Big Data
+# 2016.09.19
 
-stochastic.gradient.descent = function(convex.function,gradient.function,starting.values,data,stepsize.function,max.iter=100,min.iter=1,tol=.001,...){
+stochastic.gradient.descent = function(convex.function, gradient.function, starting.values, data, stepsize.function, function.smoothing.factor=.5, max.iter=100, min.iter=1, tol=.001, Polyak.Ruppert.burn, ...){
   #
   # Optimizes over beta vector, input to convex.function
   #
   pp = length(starting.values) 
   nn = nrow(data)
   
-  # needed data structures. 
-  output = data.frame(matrix(0,ncol=pp+4,nrow=max.iter+1))
-  beta = matrix(0,ncol=pp,nrow=max.iter+1)
+  # needed data structures. Returns "output" dataframe that includes each beta value, step size, etc. 
+  number.details = 4
+  output = data.frame(matrix(0, ncol = pp + number.details, nrow = max.iter + 1))
+  beta = matrix(0, ncol = pp, nrow = max.iter + 1)
   names(output)[1:5] = c('Step','StepSize','RandomChoice','FunctionValue','Beta0')
   
   # Starting value. Defaults as 0's
-  beta[1,]=starting.values
+  beta[1,] <- output[1, (1:pp) + number.details] <- starting.values
   
   # Initial values
-  output$FunctionValue[1] = convex.function(beta[1,],data)
+  output$FunctionValue[1] = convex.function(beta[1,],data[sample(1:nn,1),,drop=F],previous = NA,function.smoothing.factor = function.smoothing.factor)
   output$StepSize = NA
   output$RandomChoice = NA
   output$Step = 0:max.iter
-  
+
+  if(!missing(Polyak.Ruppert.burn)){min.iter = max(min.iter,Polyak.Ruppert.burn)}
   
   # Loop
   for(i in 1:max.iter){
@@ -36,21 +40,33 @@ stochastic.gradient.descent = function(convex.function,gradient.function,startin
     # Step down the hill	
     beta[i+1,] = beta[i,] - output$StepSize[i+1] * gradient 
     
-    # Measure Log Likeihood of new beta
-    output$FunctionValue[i+1] = convex.function(beta=beta[i+1,],data)
+    # Polyak-Ruppert averaging of betas. 
+    if(missing(Polyak.Ruppert.burn)){
+		output[i+1,(1:pp)+number.details] <- beta[i+1,]
+    } 
+    else {
+    		output[i+1,(1:pp)+number.details] <- (1/(i+1)) * beta[i+1,]  +  (i/(i+1)) * output[i,(1:pp)+number.details]
+    }
+    
+    # Measure target function (Log Likeihood) of new beta - using only given point (and previous, for ). Requires averaged target function
+    output$FunctionValue[i+1] = convex.function(
+      beta = as.numeric(output[i+1,(1:pp)+number.details]), 
+      data = data[choice,,drop=F], 
+      previous = output$FunctionValue[i], 
+      function.smoothing.factor = function.smoothing.factor
+      )
     
     # Stoping criteria
-    if( (norm(beta[i,,drop=F]-beta[i+1,,drop=F]) < tol) & (i >= min.iter) ){
-      output[,(1:pp)+4] <- beta
+#    if( (norm(beta[i,,drop=F]-beta[i+1,,drop=F]) < tol) & (i >= min.iter) ){
+	if( (abs(output$FunctionValue[i] - output$FunctionValue[i+1])) < tol & (i >= min.iter)){
       return(list(betahat = beta[i+1,], output=output[1:(i+1),]))
     }
   }
-  output[,(1:pp)+4] <- beta
   print('Failed to converge')
   return(list(output=output))
 }
 
-wrapper.logreg.negative.gradient = function(beta,data){
+wrapper.logreg.negative.gradient = function(beta, data){
   # function for use within optimization (minimization) function. 
   # Input: beta vector, data matrix where first column is response variable Y
   # Output: negative gradient of log binomial likelihood for logistic regression
@@ -67,7 +83,7 @@ wrapper.logreg.negative.gradient = function(beta,data){
   return(gradient)
 }
 
-wrapper.logreg.likelihood = function(beta,data){
+wrapper.logreg.likelihood = function(beta, data, previous=NA, function.smoothing.factor=NA){
   # function for use within optimization function. 
   # Input: beta vector, data matrix where first column is response variable Y
   # Output: log binomial likelihood for logistic regression
@@ -84,36 +100,83 @@ wrapper.logreg.likelihood = function(beta,data){
   return(lik)
 }
 
+wrapper.logreg.likelihood.EMA = function(beta, data, previous, function.smoothing.factor){
+  # function for use within optimization function. 
+  # Input: beta vector, data matrix where first column is response variable Y
+  # Output: log binomial likelihood for logistic regression
+  
+  # Strip Y vector off input data matrix
+  X = data[,2:ncol(data),drop=F]
+  Y = data[,1,drop=F]
+  
+  # Get predict "success" probabilities
+  w.hat = predict.logistic(X=X, beta=beta)
+  
+  # Calculate and return likelihood 
+  lik = binomial.log.likelihood(y=Y, w=w.hat)
+  
+  # Exponential moving average, alpha = function.smoothing.factor. But l1 = l1
+  if(is.na(previous)){
+  	return(lik)
+  }
+  else{
+  	return( function.smoothing.factor * lik + (1 - function.smoothing.factor) * previous )
+  }
+}
+
+
 binomial.log.likelihood = function(y, w, m=1, adjustment=.00001){
   # Returns the binomial log likelihood, short the constant term. 
   # An adjustment is added within the logs to avoid numerical instability
   sum(y*log(w+adjustment) + (m-y)*log(1-w+adjustment))
 }
 
-negative.logistic.gradient = function(y,X,w,m=1){
+negative.logistic.gradient = function(y, X, w, m=1){
   -t(X)%*%(y - w*m)
 }
 
-predict.logistic = function(X,beta){
+predict.logistic = function(X, beta){
   #equivalent to $w_i(\beta)$ in writeup
-  nn = dim(X)[1]
-  out = numeric(nn)
-  for(i in 1:nn){
-    out[i] = 1/(1+exp(-X[i,]%*%beta))
-  }
-  return(out)
+	1/(1+exp(-X%*%beta))
+  # nn = dim(X)[1]
+  # out = numeric(nn)
+  # for(i in 1:nn){
+  #   out[i] = 1/(1+exp(-X[i,]%*%beta))
+  # }
+  # return(out)
 }
 
+step.RobbinsMonro = function(t,t0=1,C,alpha){
+	if(alpha > 1 | alpha < .5){stop('check step size "a" is in reasonable range')}
+	if(C <= 0){stop('check step size "C" - currently not positive')}
+	C*(t+t0)^(-alpha)
+}
 
 ###
-#
-#
-simple.log.lik = function(beta,data){
+### 2c's simple example --------------------------------------------------------
+###
+
+# stochastic gradient descent to find mean of normal data ("simple")
+
+simple.log.lik = function(beta, data, previous=NA, function.smoothing.factor=NA){
   # likelihood for normal data with known variance = 1.
   sum(dnorm(data, mean = beta[1], sd = 1, log = TRUE))
 }
 
-negative.simple.gradient = function(beta,data){
+simple.log.lik.EMA = function(beta, data, previous, function.smoothing.factor){
+  # Exponentially-weighted moving average likelihood for normal data with known variance = 1.
+  lik = sum(dnorm(data, mean = beta[1], sd = 1, log = TRUE))
+
+	# Exponentially weighted moving average. 
+	if(is.na(previous)){
+		return(lik)
+	}
+	else{
+		return(function.smoothing.factor * lik + (1 - function.smoothing.factor) * previous)
+	}
+}
+
+negative.simple.gradient = function(beta, data){
   # Negative gradient for normal data with known variance = 1.
   sum(beta - data)
 }
--- ex02.R
+++ ex02.R
@@ -6,52 +6,243 @@ source('ex02-functions.R')
 ### 2c -------------------------------------------------------------------------------------
 ###
 
-### Simulated Dataset - Normal distributed data with known variance=1
+### Simulated Dataset - Normal distributed data with known variance=1. NOW with Exponentially moving averages (EMA) of log likelihood!!!
 source('ex01-functions.R')
 
+# Arbitrary choice of data mean 
 b = 3
+
+# Generate random N(b,1) data, cbind to ensure column vector. 
 y = cbind(rnorm(10000,mean=b,sd=1))
 
 
-# Plot different step sizes
+## Plot different step sizes
 sizes = c(.001,.01,.1)
-par(mfrow=c(1,length(sizes)))
+
+# Creates multiple-pane plot. New plots proceed down each column. 
+par(mfcol=c(2,length(sizes)))
+
+# Loop through step sizes
 for(s in sizes){
+  
+  # Set step size function to constant value s
   constant.step=function(iteration){s}
+  
+  # Run optimization
   out = stochastic.gradient.descent(
-    convex.function = simple.log.lik,
+    convex.function = simple.log.lik.EMA,
     gradient.function = negative.simple.gradient,
     starting.values = 0,
     data = y,
     stepsize.function = constant.step,
-    max.iter=10000,min.iter = 1, tol=.00001)
+    function.smoothing.factor = .1,
+    max.iter=10000,min.iter = 100, tol=.00001)
   
-  plot(out$output$Step,out$output$Beta0,type='l',xlab='Step',ylab='Parameter Estimate',main=paste('Step size =',constant.step(1)),ylim=c(0,4))
+  # For each step size, first plot log likelihood then mean estimate below it. Each with "true" value marked 
+  plot(out$output$Step, out$output$FunctionValue, 
+       type='l', 
+       xlab='Step', 
+       ylab='Log Likelihood', 
+       main=paste('Step size =',constant.step(1)) 
+       )
+  abline(h=simple.log.lik(mean(y),y), col=2)
+  plot(out$output$Step, out$output$Beta0,
+       type='l',xlab='Step',
+       ylab='Parameter Estimate',
+       main=paste('Step size =',constant.step(1)),
+       ylim=c(0,4)
+       )
   abline(h=mean(y), col=2)
 }
 
 
 
+# !! Code used with different convergence criteria, before exponentially weighted moving average of log likelihood. 
+### Real dataset -
+# 
+# wdbc = read.csv('wdbc.csv',header=F)
+# y = ifelse(wdbc[,2] == 'M',1,0)
+# X = as.matrix(cbind(1,scale(wdbc[,3:12])))
+# all.data = cbind(y,X)
+# 
+# 
+# # Plot different step sizes
+# sizes = c(.001,.01,.1,1)
+# par(mfrow=c(1,1))
+# for(s in sizes){
+#   constant.step=function(iteration,function.value){s}
+#   out = stochastic.gradient.descent(
+#     convex.function = wrapper.logreg.likelihood,
+#     gradient.function = wrapper.logreg.negative.gradient,
+#     starting.values = numeric(ncol(X)),
+#     data = all.data,
+#     stepsize.function = constant.step,
+#     max.iter=10000,tol=.0000001)
+#   if(s == sizes[1]){
+#     plot(out$output$Step,out$output$FunctionValue,type='l',xlab='Step',ylab='Log Likelihood',main='Stochastic Gradient Descent - WDBC Data',ylim=c(-4,0))
+#     abline(h= wrapper.logreg.likelihood(glm(y~X-1,family='binomial')$coefficients,all.data), col='grey')
+#   }
+#   else{
+#     lines(out$output$Step,out$output$FunctionValue,col=which(sizes == s))
+#   }
+# }
+# legend('bottomright',legend=sizes,col=1:length(sizes),lty=1,title='Step Size')
 
-### Real dataset
 
+
+
+
+
+### Real dataset - with exponential moving average log likelihood
+
+# Load data
+wdbc = read.csv('wdbc.csv',header=F)
+
+# Change binary response value to actual binary values. 
+y = ifelse(wdbc[,2] == 'M',1,0)
+
+# Select desired columns, with column of 1's for intercept term
+X = as.matrix(cbind(1,scale(wdbc[,3:12])))
+
+# Bundle both together as input into modularized functions within optimization
+# I've made all function inputs in stochastic gradient descent function accept 
+# a generic "data" varible as an input, so the code is general enough to be used again. 
+all.data = cbind(y,X)
+
+
+# Plot different step sizes
+sizes = c(.001,.01,.1,1)
+
+# side by side plots, each containing different sizes' results, but differing in alpha
+par(mfrow=c(1,2))
+for(a in c(.001,.01)){
+  for(s in sizes){
+    
+    # Set constant step size. Must be function for input in stochastic.gradient.descent
+    constant.step=function(iteration,function.value){s}
+    
+    # Optimize!
+    out = stochastic.gradient.descent(
+      convex.function = wrapper.logreg.likelihood.EMA,
+      gradient.function = wrapper.logreg.negative.gradient,
+      starting.values = numeric(ncol(X)),
+      data = all.data,
+      stepsize.function = constant.step,
+      function.smoothing.factor = a,
+      max.iter=10000,tol=1E-8)
+    
+    # Plot function value if first size, otherwise just add lines onto that plot. 
+    if(s == sizes[1]){
+      plot(out$output$Step,out$output$FunctionValue,type='l',xlab='Step',ylab='Log Likelihood',main=paste('WDBC Data - EMA alpha =',a),ylim=c(-1,0))
+      abline(h= (1/length(y))* wrapper.logreg.likelihood(glm(y~X-1,family='binomial')$coefficients,all.data), col='grey')
+    }
+    else{
+      lines(out$output$Step,out$output$FunctionValue,col=which(sizes == s))
+    }
+  }
+}
+
+# add simple legend. 
+legend('bottomright',legend=sizes,col=1:length(sizes),lty=1,title='Step Size')
+
+
+
+###
+### 2D -----------------------------------------------------------------------------------------
+###
+# Use Robbins-Monro step sizes, on different values of C>0 and alpha in [0.5,1]
+
+# Data loading and manipulation commands recopied from previous section, for convience of this section!
 wdbc = read.csv('wdbc.csv',header=F)
 y = ifelse(wdbc[,2] == 'M',1,0)
 X = as.matrix(cbind(1,scale(wdbc[,3:12])))
 all.data = cbind(y,X)
 
+# side by side plots, each containing different C and alpha
+pdf(file = 'ex02_2d.pdf')
+par(mfrow=c(3,3))
 
-constant.step=function(iteration){
-  .01
-}
-
-out = stochastic.gradient.descent(
-  convex.function = wrapper.logreg.likelihood,
-  gradient.function = wrapper.logreg.negative.gradient,
-  starting.values = numeric(ncol(X)),
-  data = all.data,
-  stepsize.function = constant.step,
-  max.iter=1000,tol=.000001)
+# loop over different alpha
+for(a in c(.85,.9,.95)){
   
-out$output[,1:5]
-plot(out$output$Step,out$output$FunctionValue,type='l',xlab='Step',ylab='Log Likelihood',main="Stochastic Gradient Descent")
+  # loop over different C 
+  for(C in c(1,2,3)){
+    
+    # Optimize!
+    out = stochastic.gradient.descent(
+      convex.function = wrapper.logreg.likelihood.EMA,
+      gradient.function = wrapper.logreg.negative.gradient,
+      starting.values = numeric(ncol(X)),
+      data = all.data,
+      stepsize.function = step.RobbinsMonro,
+      function.smoothing.factor = .005,
+      max.iter=10000,
+      tol=1E-7,
+      alpha = a,
+      C = C)
+    
+    # Plot loglikelihood value at each step and glm's likelihood. 
+      plot(out$output$Step,out$output$FunctionValue,
+           type='l',
+           xlab='Step',
+           ylab='Log Likelihood',
+           main=paste('alpha = ',a,', C = ',C,sep = ''),
+           ylim = c(-1,0),
+           xlim = c(0,10000)
+           )
+      abline(h= (1/length(y))* wrapper.logreg.likelihood(glm(y~X-1,family='binomial')$coefficients,all.data), col='grey')
+  }
+}
+dev.off()
+
+
+###
+### 2E -----------------------------------------------------------------------------------------
+###
+# Now also use Polyak-Ruppert averaging
+
+# Data loading and manipulation commands recopied from previous section, for convience of this section!
+wdbc = read.csv('wdbc.csv',header=F)
+y = ifelse(wdbc[,2] == 'M',1,0)
+X = as.matrix(cbind(1,scale(wdbc[,3:12])))
+all.data = cbind(y,X)
+
+# side by side plots, each containing different C and alpha
+pdf(file = 'ex02_2e.pdf')
+par(mfrow=c(3,3))
+
+# loop over different alpha
+for(a in c(.7,.9,.95)){
+  
+  # loop over different C 
+  for(C in c(.1,1,5)){
+    
+    # Optimize!
+    out = stochastic.gradient.descent(
+      convex.function = wrapper.logreg.likelihood.EMA,
+      gradient.function = wrapper.logreg.negative.gradient,
+      starting.values = numeric(ncol(X)),
+      data = all.data,
+      stepsize.function = step.RobbinsMonro,
+      function.smoothing.factor = .005,
+      max.iter=10000,
+      tol=1E-7,
+      Polyak.Ruppert.burn = 1000,
+      alpha = a,
+      C = C
+      )
+    
+    # Plot loglikelihood value at each step and glm's likelihood. 
+    plot(out$output$Step,out$output$FunctionValue,
+         type='l',
+         xlab='Step',
+         ylab='Log Likelihood',
+         main=paste('alpha = ',a,', C = ',C,sep = ''),
+         ylim = c(-1,0),
+         xlim = c(0,10000)
+    )
+    abline(h= (1/length(y))* wrapper.logreg.likelihood(glm(y~X-1,family='binomial')$coefficients,all.data), col='grey')
+  }
+}
+dev.off()
+
